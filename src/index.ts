959c22957f21326fe3c3196c6537f83d336c3dce
galkatz373 <galkatz373@gmail.com>
2022-06-05 22:51:57 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,7 @@ require('dotenv').config();
 
 const sendEmail = async () => {
   const transporter = createTransport({
-    host: 'smtp.gmail.com',
     service: 'gmail',
-    port: 587,
-    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
